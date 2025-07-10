3a2f42d6effa5a98799e5f7757fb8f3812e6e38e
White, Otto <otto.white20@imperial.ac.uk>
2025-07-10 12:09:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+def get_completion(client, prompt, model="openai/gpt-4o"):
+    messages = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    completion = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+    return completion.choices[0].message.content
+
+def main():
+    load_dotenv()  # take environment variables
+    openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
+    client = OpenAI(
+      base_url="https://openrouter.ai/api/v1",
+      api_key=openrouter_api_key,
+    )
+
+    message = get_completion(client, "What is the meaning of life?")
+    
+    print(message)
+
+if __name__ == "__main__":
+    main()
+
