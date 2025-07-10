4954e7d66a00dbb2fbea8318cf14cdb07467732c
White, Otto <otto.white20@imperial.ac.uk>
2025-07-10 15:49:26 +0100

    Make use langgraph instead of openai client

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
-from openai import OpenAI
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
 # Check out the models here: https://openrouter.ai/models
 # https://openrouter.ai/models
 # Check out my usage here: https://openrouter.ai/activity
 # Models I've used
 # openai/gpt-4o
-
-def get_completion(client, prompt, model="openai/gpt-4o"):
-    messages = [
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ]
-    completion = client.chat.completions.create(
-        model=model,
-        messages=messages
-    )
-    return completion.choices[0].message.content
-
 def main():
-    load_dotenv()  # take environment variables
-    openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
-    client = OpenAI(
-      base_url="https://openrouter.ai/api/v1",
-      api_key=openrouter_api_key,
+    load_dotenv()
+
+    llm = ChatOpenAI(
+        model="mistralai/mistral-7b-instruct:free",
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.environ["OPENROUTER_API_KEY"]
     )
 
-    message = get_completion(client, "What is the meaning of life?")
-    
-    print(message)
+    response = llm.invoke("What is the meaning of life?")
+    print(response)
 
 if __name__ == "__main__":
     main()
