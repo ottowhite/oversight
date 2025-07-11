3c4bb02f49d35014af24c931eebbd61294ea3ed2
White, Otto <otto.white20@imperial.ac.uk>
2025-07-11 16:42:03 +0100

    Add embeddings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 # Check out the models here: https://openrouter.ai/models
 # https://openrouter.ai/models
@@ -10,15 +10,23 @@ from langchain_openai import ChatOpenAI
 # openai/gpt-4o
 def main():
     load_dotenv()
-
     llm = ChatOpenAI(
         model="mistralai/mistral-7b-instruct:free",
         base_url="https://openrouter.ai/api/v1",
         api_key=os.environ["OPENROUTER_API_KEY"]
     )
 
-    response = llm.invoke("What is the meaning of life?")
+    embedding_model = OpenAIEmbeddings(
+        model="text-embedding-3-small",
+        api_key=os.environ["OPENAI_API_KEY"]
+    )
+
+    response = llm.invoke("How do I say Jinnan in chinese?")
     print(response)
+
+    embedding = embedding_model.embed_query("How do I say Jinnan in chinese?")
+    print(len(embedding))
+
 
     # TODO: Implement a langgraph chain
     # https://medium.com/@699580621meliga/build-your-first-langraph-chain-with-openrouter-mistral-7b-c7b47fdd4ced
